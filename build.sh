949bb14f89435c4196d21ded9ec8197f4a711137
Aarjav Jain <117491745+AarjavJain101@users.noreply.github.com>
2023-06-04 18:50:14 -0600

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,20 @@
+THREADS=8
+
+if [ ! -d fftw-3.3.10 ] && [ ! -d portaudio ]; then 
+	curl https://www.fftw.org/fftw-3.3.10.tar.gz | tar --extract -z -f - 
+	curl http://files.portaudio.com/archives/pa_stable_v190700_20210406.tgz | tar --extract -z -f - 
+	cd fftw-3.3.10
+	./configure
+	make -j${THREADS} 
+	cd ../portaudio
+	./configure
+	make -j${THREADS} 
+fi
+
+if [ -f portaudio/.libs/libportaudio.a ]; then 
+	PORTAUDIO_LIB="libportaudio.a" 
+else 
+	PORTAUDIO_LIB="libportaudio.dll.a" 
+fi
+
+g++ -o Beat_Tracking Beat_Tracking.cpp -Iportaudio/include -Ifftw-3.3.10/api -pthread -Lportaudio/lib/.libs -Lfftw-3.3.10/.libs -l:${PORTAUDIO_LIB} -lm -l:libfftw3.a
